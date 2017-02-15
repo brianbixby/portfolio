45a19e77128c7a27bdc87f28a4407616558acc25
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-02-14 18:51:42 -0800

    sidenav and scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function() {
     $('.col-sm-6').matchHeight();
 });
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
