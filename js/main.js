0c5c2343a1c89dd1e071747cfb30a86848032a65
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-02-14 19:26:01 -0800

    adding easteregg in console.log

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$(document).ready(function() {
+  var link = document.querySelectorAll('#console a');
+  for(var i = 0; i < link.length; i++){
+    console.log(link[i].href);
+  };
+});
+
+
+
 $(function() {
     $('.col-sm-6').matchHeight();
 });
@@ -8,3 +17,8 @@ $('a').click(function(){
     }, 500);
     return false;
 });
+
+
+
+
+// ,window.joinUs=function(){return console.log("%cWe're excited to have you! %c☃",e(!1,"#d22"),e(!1,"#333")),window.location.href="/_/_/about/careers/",""}}()}
